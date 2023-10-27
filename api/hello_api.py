8ffe5c9bf0dd60f5cdbd471337777c95c898c112
yukitech <yuki838ikrkj@gmail.com>
2023-10-27 15:59:17 +0900

    first commit

---
+++ api/hello_api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
